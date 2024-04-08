40eef4e14d66bbc52ffc47159b5025f1126093d0
Jaeho Cho <jaeho2025@gmail.com>
2024-04-08 01:27:59 -0400

    typer

---
+++ .bash_aliases
@@ -0,0 +1,4 @@
+alias "Hi!"="echo 'My name is Jaeho Cho'"
+alias "I_am_an_Electrical_Engineering_Major"="echo '@ The Cooper Union in New York City'"
+alias "Driven_by_dual_passions_in_robotics_and_medicine"="echo 'I am in pursuit of the invaluable skills that only come with experience'"
+alias "Feel_free_to_reach_out_and_say_hi!"="echo 'phone: (201) 406 5974 | email: jaeho2025@gmail.com'"
---
+++ typer.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+# failsafe: move mouse to any one of the four corners of the primary monitor
+
+import pyautogui
+import time
+
+delay = 2
+while delay > 0:
+	print(f"select window to type in | will start in {delay:.1f} seconds")
+	time.sleep(0.1)
+	delay -= 0.1
+print("typing...")
+
+strings = [
+    "Hi!",
+    "I_am_an_Electrical_Engineering_Major",
+    "Driven_by_dual_passions_in_robotics_and_medicine",
+    "Feel_free_to_reach_out_and_say_hi!"
+]
+
+interval = 0.03   # In Seconds
+
+pause = 3
+for string in strings:
+	pyautogui.write(string, interval=interval)
+	time.sleep(1)
+	pyautogui.write("\n")
+	time.sleep(pause)
+
+print("complete")
